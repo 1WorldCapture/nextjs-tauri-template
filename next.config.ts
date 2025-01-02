5d4f0ba02f83f2a81d5af3770d9c2d91d47441cf
1WorldCapture <ll_nwpu@qq.com>
2025-01-02 14:04:03 +0800

    Enhance Tauri integration: Update Next.js configuration, add Tauri dependencies, and set up initial Tauri project structure with icons and configuration files.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+const internalHost = process.env.TAURI_DEV_HOST || "localhost";
+const internalPort = process.env.TAURI_DEV_PORT || "3000";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProduction
+    ? undefined
+    : `http://${internalHost}:${internalPort}/`,
 };
 
 export default nextConfig;
